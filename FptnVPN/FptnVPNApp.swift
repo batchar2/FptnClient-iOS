3fd179daffdac21f6455be51cfb8905ed8108ef0
Stas Skokov <7090stas@gmail.com>
2025-06-14 16:11:22 +1000

    Initial Commit

---
+++ FptnVPN/FptnVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  FptnVPNApp.swift
+//  FptnVPN
+//
+//  Created by Stanislav on 14/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct FptnVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FptnVPNTests/FptnVPNTests.swift
@@ -0,0 +1,17 @@
+//
+//  FptnVPNTests.swift
+//  FptnVPNTests
+//
+//  Created by Stanislav on 14/6/2025.
+//
+
+import Testing
+@testable import FptnVPN
+
+struct FptnVPNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
