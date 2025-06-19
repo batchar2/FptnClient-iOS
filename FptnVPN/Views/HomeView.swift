c4d80a1e010f6ced1cf87e588e6f8bb89da3ecc0
Stas Skokov <7090stas@gmail.com>
2025-06-19 22:11:31 +1000

    home screen

--- FptnVPN/Views/HomeView.swift
+++ FptnVPN/Views/HomeView.swift
@@ -9,77 +9,114 @@
 import SwiftUI
 
 
+
 struct HomeView: View {
     @State private var isConnected = false
     @State private var selectedMode = "Auto"
+    @State private var connectionDuration = 5
+    @State private var serverName = "USA–NewYork (🇺🇸)"
+    @State private var downloadSpeed = "7,75 Kbps"
+    @State private var uploadSpeed = "20,75 Kbps"
     
     var body: some View {
         VStack {
             Spacer()
-                        
-            // Power button icon
-            Image(systemName: "power")
-                .resizable()
-                .frame(width: 120, height: 120)
-                .foregroundColor(isConnected ? .green : .gray)
-                .padding()
-                .onTapGesture {
-                    withAnimation {
-                        isConnected.toggle()
+            
+            // Time
+            if isConnected {
+                Text("Время подключения")
+                    .foregroundColor(.white)
+                Text(String(format: "00:00:%02d", connectionDuration))
+                    //.font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding(.bottom, 10)
+            }
+            
+            // Кнопка включения
+            Button(action: {
+                withAnimation {
+                    isConnected.toggle()
+                    if isConnected {
+                        connectionDuration = 0
                     }
                 }
-                        
-            // Connection status text
-            Text(isConnected ? "Connected" : "Disconnected")
-                .foregroundColor(isConnected ? .green : .yellow)
+            }, label: {
+                Image(isConnected ? "toggle_button_on" : "toggle_button_off")
+                    .resizable()
+                    .frame(width: 180, height: 180)
+                    .padding()
+            })
+            
+            // Status
+            Text(isConnected ? "Подключено" : "Отключено")
+                .foregroundColor(isConnected ? .yellow : .gray)
                 .font(.headline)
-                .padding(.bottom, 40)
-                        
-            // Dropdown menu for mode selection
-            Menu {
-                Button("Auto") { selectedMode = "Auto" }
-                Button("Manual") { selectedMode = "Manual" }
-                // Add more options if needed
-            } label: {
+                .padding(.bottom, 4)
+            
+            // Server
+            if isConnected {
+                Text("Сервер: \(serverName)")
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+                    .padding(.bottom, 20)
+            }
+            
+            // Speed
+            if isConnected {
                 HStack {
-                    Image(systemName: "shield")
-                    Text(selectedMode)
+                    Image(systemName: "arrow.down.to.line.alt")
+                    Text(downloadSpeed)
                     Spacer()
+                    Text(uploadSpeed)
+                    Image(systemName: "arrow.up.to.line.alt")
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.purple.opacity(0.3))
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+
+
+            if !isConnected {
+                Menu {
+                    Button("Auto") { selectedMode = "Auto" }
+                    Button("Manual") { selectedMode = "Manual" }
+                } label: {
+                    HStack {
+                        Image(systemName: "shield")
+                        Text(selectedMode)
+                        Spacer()
                         Image(systemName: "chevron.down")
                     }
-                .padding()
-                .frame(height: 50)
-                .frame(maxWidth: .infinity)
-                .background(Color.white)
-                .cornerRadius(20)
-                .shadow(radius: 2)
+                    .padding()
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 2)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
-                    
             Spacer()
-                        
-            // Bottom navigation bar
+            
             HStack {
                 Spacer()
-                
                 VStack {
                     Image(systemName: "house")
-                    Text("Home")
+                    Text("Главная")
                         .font(.caption)
                 }
                 Spacer()
-                
                 VStack {
                     Image(systemName: "gear")
-                    Text("Settings")
+                    Text("Настройки")
                         .font(.caption)
                 }
-                
                 Spacer()
-                
                 VStack {
                     Image(systemName: "square.and.arrow.up")
-                    Text("Share")
+                    Text("Поделиться")
                         .font(.caption)
                 }
                 Spacer()
@@ -88,7 +125,7 @@ struct HomeView: View {
             .padding()
             .background(Color.black.opacity(0.9))
         }
-        .background(Color.appBackground) // Dark purple background
+        .background(Color.appBackground)
         .edgesIgnoringSafeArea(.bottom)
     }
 }
